0950f1c6735e3c2f1aaefe11d9272e4fb508e45a
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-04-18 23:57:04 +0600

    54-7 Create React Form And Post API And Send Data To The Server

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,38 @@ function App() {
       .then((res) => res.json())
       .then((data) => setUser(data));
   }, []);
+
+  const handleAddUser = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+
+    const user = { name, email };
+
+    fetch("http://localhost:5000/user", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
   return (
     <>
       <h1>User Management System</h1>
       <h3>Total User: {users.length}</h3>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" placeholder="name" /> <br />
+        <input type="email" name="email" placeholder="email" /> <br />
+        <input type="submit" value="Submit" />
+      </form>
+
+      {users.map((user) => (
+        <p key={user.id}>
+          {user.id}: {user.name}: {user.email}
+        </p>
+      ))}
     </>
   );
 }
